78f8ed280e728ef73384d839f94ebb72ecbfce21
376305 <shishuaiyun@deppon.com>
2020-10-29 17:21:52 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // 无法使用console，如果使用生产环境会警告
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off' // 无法使用debugger，如果使用生产环境会警告
   }
 }
